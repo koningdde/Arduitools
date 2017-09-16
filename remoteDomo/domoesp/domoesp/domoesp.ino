61c68275a7c279dd0af0972a40d4a1de87d66747
koningdde <koningdde@gmail.com>
2017-09-16 11:10:38 +0200

    add

--- remoteDomo/domoesp/domoesp/domoesp.ino
+++ remoteDomo/domoesp/domoesp/domoesp.ino
@@ -54,68 +54,7 @@ WiFiClient espClient;
 PubSubClient client(espClient);
 
 
-void setup_wifi() {
 
-  delay(10);
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  randomSeed(micros());
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-}
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i=0;i<length;i++) {
-    Serial.print((char)payload[i]);      
-  }
-  Serial.println();
-  if ((char)payload[0] == unitId){
-   
-    switch ((char)payload[1]){
-      case 'r':
-       relayOut( (char)payload[2],(char)payload[3] );
-       break;
-    }//end switch case
-    }//enf first iff
-  
-  else {
-      Serial.println("reject unitId");
-      }
-}
-
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    if (client.connect(clientId)) {
-      Serial.println("connected");
-      client.subscribe("domoticz/arduino");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
-}
 
 void setup() {
   pinMode(inputPin, INPUT_PULLUP);     // declare sensor as input
@@ -242,4 +181,67 @@ void lightOut(int idx, char cmd[]){
       client.publish("domoticz/in",data2);
       // ... and resubscribe
       client.subscribe("domoticz/arduino"); 
+}
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i=0;i<length;i++) {
+    Serial.print((char)payload[i]);      
+  }
+  Serial.println();
+  if ((char)payload[0] == unitId){
+   
+    switch ((char)payload[1]){
+      case 'r':
+       relayOut( (char)payload[2],(char)payload[3] );
+       break;
+    }//end switch case
+    }//enf first iff
+  
+  else {
+      Serial.println("reject unitId");
+      }
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect(clientId)) {
+      Serial.println("connected");
+      client.subscribe("domoticz/arduino");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
 }
